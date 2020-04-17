4656e41ab0c0f8e16b1b4c3189bd6ed2a97339bd
Thomas <thomas.dulcamara@alumni.univ-avignon.fr>
2020-04-17 16:32:05 +0200

    Add jeuxController

---
+++ src/application/JeuxController.java
@@ -0,0 +1,10 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class JeuxController {
+
+	public void clickOnCase(ActionEvent event) {
+		System.out.println("Salut !!! ");
+	}
+}
