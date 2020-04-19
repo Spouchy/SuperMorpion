f536e31aee43d3c951e8376b90e9e2b44aea07be
Spouchy <spouchy@hotmail.fr>
2020-04-19 15:40:12 +0200

    images modifier

--- src/application/PlateauMorpion.java
+++ src/application/PlateauMorpion.java
@@ -17,8 +17,8 @@ public class PlateauMorpion {
 	private static int DIFFICULTE = 0;
 	private static boolean IN_TRAINING = false;
 	public boolean siJeuxEnCours;
-	private String imgJoueur1 = "file:src/application/images/teteSpiderman.jpg";
-	private String imgJoueur2 = "file:src/application/images/teteBatman.png";
+	private String imgJoueur1 = "file:src/application/images/spiderpng2.png";
+	private String imgJoueur2 = "file:src/application/images/batman_png2.png";
 	private int tourJoueur = 1;
 	private int[][] matricePlateau =
 	    {
